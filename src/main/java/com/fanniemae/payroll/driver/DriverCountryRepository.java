01a74a40229f6bbc9fc52c27a743b1dc739add83
mattlharding <matthew_harding@fanniemae.com>
2017-07-13 16:49:26 -0400

    Fourth Lab

---
+++ src/main/java/com/fanniemae/payroll/driver/DriverCountryRepository.java
@@ -0,0 +1,17 @@
+package com.fanniemae.payroll.driver;
+
+import java.util.ArrayList;
+
+import com.fanniemae.payroll.dao.CityRepository;
+import com.fanniemae.payroll.model.City;
+import com.fanniemae.payroll.model.Country;
+
+public class DriverCountryRepository {
+
+	public static void main (String[] args) {
+		CountryRepository dao = new CountryRepository ();
+		((ArrayList<Country>) dao.findAll()).forEach(System.out::println);
+	}
+	
+	
+}
