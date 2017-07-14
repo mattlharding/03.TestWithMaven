6992798d4e4c802572ffcc884e6fb3cdd2c098eb
mattlharding <matthew_harding@fanniemae.com>
2017-07-14 13:39:38 -0400

    Delete item from MSSQL

--- src/main/java/com/fanniemae/payroll/dao/AbstractSQLDAO.java
+++ src/main/java/com/fanniemae/payroll/dao/AbstractSQLDAO.java
@@ -9,43 +9,44 @@ import org.apache.commons.dbcp.BasicDataSource;
 
 public abstract class AbstractSQLDAO {
 
-	public void process(String sqlStatement) {
+	private BasicDataSource ds = new BasicDataSource();
 
-		BasicDataSource ds = new BasicDataSource();
-
+	private Connection con = null;
+	private Statement stmt = null;
+	private ResultSet rs = null;
+	{
 		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		ds.setUsername("root");
 		ds.setPassword("Hexaware!");
 		ds.setUrl("jdbc:mysql://localhost:3306/world?useSSL=false");
+	}
 
-		Connection con = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+	public void modify(String sqlstatement) {
+		try (Connection con = ds.getConnection();)
 
-		try {
-			con = ds.getConnection();
+		{
 			stmt = con.createStatement();
-			rs = stmt.executeQuery(sqlStatement);
-			while (rs.next() == true) {
-				results (rs);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (stmt != null)
-					stmt.close();
-				if (con != null)
-					con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			stmt.executeUpdate(sqlstatement);
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 
 	}
 
-	
-	public abstract void results (ResultSet rs) throws SQLException;
+	public void process(String sqlStatement) {
+
+		try (Connection con = ds.getConnection();) {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sqlStatement);
+			while (rs.next() == true) {
+				results(rs);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public abstract void results(ResultSet rs) throws SQLException;
 }
